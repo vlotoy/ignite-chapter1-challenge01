3b71e704548c9d77292978a9db14487bbb1976df
Victor Loyola <victor.tostes@gmail.com>
2021-08-24 12:43:36 -0400

    Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    const newTask = {
+      id: Math.floor(Math.random() * 100),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    
+    if (!newTaskTitle) return
+
+    setTasks([...tasks, newTask])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const checkedTasks = tasks.map(task => task.id === id ? {
+      	...task,
+        isComplete: !task.isComplete
+    } : task)
+
+    setTasks(checkedTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id)
+    
+    setTasks(filteredTasks)
   }
 
   return (
